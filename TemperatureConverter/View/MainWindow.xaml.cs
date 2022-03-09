6be76fdbe209b9f620ed6f601fceca263af33c7c
pieterjan-dubois <pieterjan.dubois@student.ucll.be>
2022-03-09 13:02:31 +0100

    my first commit

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace View
         {
             InitializeComponent();
         }
+
+        private void convertToCelsius(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "You clicked the button", "Clicked!");
+            textBox.Text = "You clicked the buttonxd";
+        }
     }
 }
